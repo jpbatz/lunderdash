9ef599886d17930e4fc01ed1bca4dcfe2993a25e
jph <jph@hawaii.edu>
2014-10-11 16:40:53 -1000

    created setup files

--- lunderdash.js
+++ lunderdash.js
@@ -1,1 +1,9 @@
-var _ = require('lodash');
+// create your project file to "whatever.js" for example cholo.js
+// put this code in here!
+ 
+module.exports = {
+  // compact: function(){},
+  // difference: function(){},
+  //etc...
+  test: function(){console.log("test! rename this file to your project name")}
+};
---
+++ runner.js
@@ -0,0 +1,5 @@
+// new file named runner.js
+var _ = require('./lunderdash'); // your "whatever" file name (no .js is needed)
+ 
+var result = _.test();
+console.log(result);
