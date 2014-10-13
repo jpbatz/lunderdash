8888e13975a8692cd6694d706aa38f0fc7b473d9
jph <jph@hawaii.edu>
2014-10-13 03:09:10 -1000

    Array union works but need more error traps

--- lunderdash.js
+++ lunderdash.js
@@ -4,6 +4,30 @@
 module.exports = {
   // compact: function(){},
   // difference: function(){},
+  union: function() {
+    var temp = [];
+    console.log(arguments.length);
+    for(var i=0; i<arguments.length; i++) {
+      temp = temp.concat(arguments[i]);
+    }
+
+    console.log(temp.length);
+
+    var result = [];
+    for(var j=0; j<temp.length; j++) {
+    // compare item in results with with each values in temp
+      if(result.indexOf(temp[j]) === -1) {
+        // if no match, push onto results
+        result.push(temp[j]);
+      }
+    }
+
+    // return result array
+    return result;
+  },
+  
   //etc...
-  test: function(){console.log("test! rename this file to your project name")}
+  test: function(){
+    console.log("test! rename this file to your project name");
+  }
 };
